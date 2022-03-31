a7942ae532303121018a1a5dcbde37254cf70a1b
Maaida10 <maaida1999@gmail.com>
2022-03-31 01:15:43 -0400

    new changes have been made to sample_repo.py

--- sample_repo.py
+++ sample_repo.py
@@ -6,5 +6,5 @@ This is a sample file for Exercise 4 that prints out a hello message.
 def hello_message(person):
    print("Hello" + " " + person)
 
-print "Who are you?"
+print("Hello World!")
 hello_message("Maaida")
