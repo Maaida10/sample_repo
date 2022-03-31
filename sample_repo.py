0301949d8cb7d6d0580075115776d59fb03ddcee
Maaida10 <maaida1999@gmail.com>
2022-03-31 01:25:46 -0400

    Maaida has modified sample_repo.py

--- sample_repo.py
+++ sample_repo.py
@@ -4,8 +4,11 @@ This is a sample file for Exercise 4 that prints out a hello message.
 """
 
 def hello_message(person):
+   print("My name is" + " " + person)
    print("Hello" + " " + person)
 
-
+print("What's your name?")
 hello_message("Maaida!")
 hello_message("World!")
+
+
