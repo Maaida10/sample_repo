4e27d48740335cd48183bc70cd1e750f6f1335a5
Maaida10 <maaida1999@gmail.com>
2022-03-31 01:05:35 -0400

    added sample_repo.py

---
+++ sample_repo.py
@@ -0,0 +1,9 @@
+""" This is a sample file for Exercise 4 located in my local repository. """
+
+from email import message
+
+
+def hello_message(person):
+   print("Hello" + " " + person)
+
+hello_message("Maaida")
